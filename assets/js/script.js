6a7436640e73b128be41635c64274be8673ea0be
andrewr50 <51382718+andrewr50@users.noreply.github.com>
2023-07-09 19:29:00 -0500

    functions built. app functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,10 @@ var searchHistoryContainer = document.querySelector('#history');
 
 var searchHistory = [];
 
+// extend dayjs to work with api
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
+
 //render search history from recent to old
 function renderSearchHistory() {
   searchHistoryContainer.innerHTML = '';
@@ -18,7 +20,6 @@ function renderSearchHistory() {
   for (var i = searchHistory.length - 1; i >= 0; i--) {
     var btn = document.createElement('button');
     btn.setAttribute('type', 'button');
-    btn.setAttribute('today', 'forecast');
     btn.classList.add('history-btn', 'btn-history');
 
     btn.setAttribute('data-search', searchHistory[i]);
@@ -46,12 +47,11 @@ function initSearchHistory() {
 }
 
 function renderCurrentWeather(city, weather) {
+  var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
   var date = dayjs().format('M/D/YYYY');
-  var tempF = weather.main.temp;
+  var temp = weather.main.temp;
   var windMph = weather.wind.speed;
   var humidity = weather.main.humidity;
-  var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
-  var iconDescription = weather.weather[0].description || weather[0].main;
 
   var card = document.createElement('div');
   var cardBody = document.createElement('div');
@@ -61,21 +61,15 @@ function renderCurrentWeather(city, weather) {
   var windEl = document.createElement('p');
   var humidityEl = document.createElement('p');
 
-  card.setAttribute('class', 'card');
-  cardBody.setAttribute('class', 'card-body');
   card.append(cardBody);
 
   heading.setAttribute('class', 'h3 card-title');
-  tempEl.setAttribute('class', 'card-text');
-  windEl.setAttribute('class', 'card-text');
-  humidityEl.setAttribute('class', 'card-text');
 
   heading.textContent = `${city} (${date})`;
   weatherIcon.setAttribute('src', iconUrl);
-  weatherIcon.setAttribute('alt', iconDescription);
   weatherIcon.setAttribute('class', 'weather-img');
   heading.append(weatherIcon);
-  tempEl.textContent = `Temp: ${tempF}°F`;
+  tempEl.textContent = `Temp: ${temp}°F`;
   windEl.textContent = `Wind: ${windMph} MPH`;
   humidityEl.textContent = `Humidity: ${humidity} %`;
   cardBody.append(heading, tempEl, windEl, humidityEl);
@@ -87,11 +81,11 @@ function renderCurrentWeather(city, weather) {
 function renderForecastCard(forecast) {
   var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
   var iconDescription = forecast.weather[0].description;
-  var tempF = forecast.main.temp;
+  var temp = forecast.main.temp;
   var humidity = forecast.main.humidity;
   var windMph = forecast.wind.speed;
 
-  // create elements for cards
+  // elements for weather cards
   var col = document.createElement('div');
   var card = document.createElement('div');
   var cardBody = document.createElement('div');
@@ -101,50 +95,39 @@ function renderForecastCard(forecast) {
   var windEl = document.createElement('p');
   var humidityEl = document.createElement('p');
 
+  // add elements to DOM
   col.append(card);
   card.append(cardBody);
   cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
 
-  col.setAttribute('class', 'col-md');
-  col.classList.add('five-day-card');
-  card.setAttribute('class', 'card bg-primary h-100 text-white');
-  cardBody.setAttribute('class', 'card-body p-2');
-  cardTitle.setAttribute('class', 'card-title');
-  tempEl.setAttribute('class', 'card-text');
-  windEl.setAttribute('class', 'card-text');
-  humidityEl.setAttribute('class', 'card-text');
-
-  // Add content to elements
+  // card content
   cardTitle.textContent = dayjs(forecast.dt_txt).format('M/D/YYYY');
   weatherIcon.setAttribute('src', iconUrl);
   weatherIcon.setAttribute('alt', iconDescription);
-  tempEl.textContent = `Temp: ${tempF} °F`;
+  tempEl.textContent = `Temp: ${temp} °F`;
   windEl.textContent = `Wind: ${windMph} MPH`;
   humidityEl.textContent = `Humidity: ${humidity} %`;
-
+  // add weather to DOM
   forecastContainer.append(col);
 }
 
 // Function to display 5 day forecast.
 function renderForecast(dailyForecast) {
-  // Create unix timestamps for start and end of 5 day forecast
-  var startDt = dayjs().add(1, 'day').startOf('day').unix();
-  var endDt = dayjs().add(6, 'day').startOf('day').unix();
-
+  // feed time to api that it can read for 5 day forecast
+  var startDate = dayjs().add(1, 'day').startOf('day').unix();
+  var endDate = dayjs().add(6, 'day').startOf('day').unix();
+  // create heading
   var headingCol = document.createElement('div');
   var heading = document.createElement('h4');
-
-  headingCol.setAttribute('class', 'col-12');
-  heading.textContent = '5-Day Forecast:';
+  // add heading
   headingCol.append(heading);
 
   forecastContainer.innerHTML = '';
   forecastContainer.append(headingCol);
-
+  //render weather cards
   for (var i = 0; i < dailyForecast.length; i++) {
 
-    if (dailyForecast[i].dt >= startDt && dailyForecast[i].dt < endDt) {
-
+    if (dailyForecast[i].dt >= startDate && dailyForecast[i].dt < endDate) {
       if (dailyForecast[i].dt_txt.slice(11, 13) == "12") {
         renderForecastCard(dailyForecast[i]);
       }
@@ -152,7 +135,7 @@ function renderForecast(dailyForecast) {
   }
 }
 
-function renderItems(city, data) {
+function renderWeather(city, data) {
   renderCurrentWeather(city, data.list[0], data.city.timezone);
   renderForecast(data.list);
 }
@@ -162,27 +145,28 @@ function fetchWeather(location) {
   var { lon } = location;
   var city = location.name;
 
-  var apiUrl = `${weatherApiUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
+  var weatherUrl = `${weatherApiUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
 
-  fetch(apiUrl)
+  try {
+  fetch(weatherUrl)
     .then(function (res) {
       return res.json();
     })
-    .then(function (data) {
-      renderItems(city, data);
+    .then(function (data) 
+      {renderWeather(city, data);
     })
-    .catch(function (err) {
-      console.error(err);
-    });
+  } catch {
+    console.error(err);
+  }
 }
 
 function fetchCoords(search) {
+  // this function is deprecated per api source, may break later
   var apiUrl = `${weatherApiUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
 
+  try {
   fetch(apiUrl)
-    .then(function (res) {
-      return res.json();
-    })
+    .then(function (res) { return res.json(); })
     .then(function (data) {
       if (!data[0]) {
         alert('Location not found');
@@ -191,13 +175,12 @@ function fetchCoords(search) {
         fetchWeather(data[0]);
       }
     })
-    .catch(function (err) {
-      console.error(err);
-    });
+  } catch { 
+    console.error(err);
+  }
 }
 
 function handleSearchFormSubmit(e) {
-  // Don't continue if there is nothing in the search form
   if (!searchInput.value) {
     return;
   }
@@ -209,7 +192,6 @@ function handleSearchFormSubmit(e) {
 }
 
 function handleSearchHistoryClick(e) {
-  // Don't do search if current elements is not a search history button
   if (!e.target.matches('.btn-history')) {
     return;
   }
